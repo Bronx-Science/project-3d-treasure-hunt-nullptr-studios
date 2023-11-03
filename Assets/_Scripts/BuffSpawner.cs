4a632c7202646930821d24d176df9475c82f4786
AstralTurtle <jamesacs329@gmail.com>
2023-11-03 11:06:05 -0400

    buff spawner class, buff and treasure spawner gameobjects

---
+++ Assets/_Scripts/BuffSpawner.cs
@@ -0,0 +1,74 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class BuffSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] treasurePrefabs = new GameObject[10];
+    [SerializeField] ArrayList spawnedTreasures = new ArrayList();
+    
+    int[] spawnedPrefabs;
+    [SerializeField] int maxTreasures = 3;
+    [SerializeField] int treasureCount = 0;
+    [SerializeField] float spawnDelay = 15f;
+    // public for testing purposes
+    public float spawnTimer = 0f;
+    [SerializeField] float bounds = 1000f;
+
+    public void SpawnTreasure()
+    {
+        if (treasureCount >= maxTreasures)
+        {
+            return;
+        }
+        // go to random position
+        float x = Random.Range(0, bounds);
+        float z = Random.Range(0, bounds);
+        transform.position = new Vector3(x, 100f, z);
+        // pick a random treasure prefab
+        int treasureIndex = Random.Range(0, treasurePrefabs.Length);
+        if (spawnedPrefabs[treasureIndex] == 1)
+        {
+            SpawnTreasure();
+            return;
+        }
+        // raycast down to get treasure spawn position
+        RaycastHit hit;
+        Vector3 spawnPosition;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 100f))
+        {
+           spawnPosition  = hit.point;
+           GameObject treasure = Instantiate(treasurePrefabs[treasureIndex], spawnPosition, Quaternion.identity);
+            spawnedTreasures[treasureIndex] = treasure;
+            spawnedPrefabs[treasureIndex] = 1;
+            treasureCount++;
+        }
+        else
+        {
+            Debug.LogError("No ground found");
+            SpawnTreasure();
+            return;
+        }
+        
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       spawnedPrefabs = new int[treasurePrefabs.Length]; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnDelay){
+            spawnTimer = 0f;
+            if (treasureCount < maxTreasures)
+            {
+                SpawnTreasure();
+            }
+        }
+    }
+}
