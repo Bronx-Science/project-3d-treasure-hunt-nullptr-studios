26d37c72d684f101f60020ab37c5a1964b0a1315
AstralTurtle <jamesacs329@gmail.com>
2023-11-07 11:12:41 -0500

    added TMPro, UI elements

---
+++ Assets/_Scripts/Words.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Words : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(wordbank.getWord());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
