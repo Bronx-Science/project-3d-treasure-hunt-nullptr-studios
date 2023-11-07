d1d92d6f7f3f957dd8f844dfed0c5bd9b798e55c
AstralTurtle <jamesacs329@gmail.com>
2023-11-07 14:27:18 -0500

    created wordbank to call api, word.cs as word manager, current sets display string to empty char array

--- Assets/_Scripts/Words.cs
+++ Assets/_Scripts/Words.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Words : MonoBehaviour
 {
+    
+    public string word = "";
+    public TMPro.TextMeshProUGUI wordText;
+
+    public char[] revealedLetters;
+    public int lettersRevealed = 0;
+
+    public int wordLength = 0;
+
+    public string displayString = "Getting Word...";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(wordbank.getWord());
+        wordbank.instance.getWordFromApi();
     }
 
     // Update is called once per frame
     void Update()
     {
+         updateDisplay();
+        wordText.text = displayString;
+       
+    }
+
+    public void getWord(){
+        word = wordbank.instance.word;
+        Debug.Log(word);
+        init();
+    }
+
+    public void init(){
+        wordLength = word.Length;
+        revealedLetters = new char[26];
+        displayString = "";
+        for(int i = 0; i < wordLength; i++){
+            displayString += "_ ";
+        }
+    }
+
+    public void updateDisplay(){
+        displayString = "";
+        for (int i = 0; i < wordLength; i++)
+        {
+            char toAdd = '_';
+            foreach (char letter in revealedLetters)
+            {
+                if (letter == word[i])
+                {
+                    toAdd = letter;
+                    break;
+                }
+            }
+            displayString += toAdd + " ";
+        }
+
         
     }
+
+    public void revealLetter(char letter){
+        revealedLetters[lettersRevealed] = letter;
+        lettersRevealed++;
+    }
+
+    public void guessWord(string guess){
+        if (guess = word)
+        {
+            Debug.Log("You Win!");
+            displayString = "Getting Word..."
+            wordbank.instance.getWordFromApi();
+
+        }
+        else
+        {
+            Debug.Log("You Lose!");
+        }
+    }
+
+
+
+
 }
